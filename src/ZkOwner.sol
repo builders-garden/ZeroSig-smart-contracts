4475bf2e9f4ddd2da673735f479f75ec4ac25314
francescocirulli <63744530+francescocirulli@users.noreply.github.com>
2025-06-14 14:33:13 +0200

    update ZkOwner.sol contract

---
+++ src/ZkOwner.sol
@@ -0,0 +1,40 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.17;
+
+import { ISignatureValidator } from "./Safe/ISignatureValidator.sol";
+
+contract ZkOwner is ISignatureValidator {
+  address public safeProxyAddress;
+
+  function initialize(address _safeProxyAddress) external {
+    safeProxyAddress = _safeProxyAddress;
+  }
+
+  function isValidSignature(
+    bytes32 _hash,
+    bytes memory _signature
+  ) external view override returns (bytes4) {
+    return EIP1271_MAGIC_VALUE;
+  }
+
+  function getNonce() external view returns (uint256) {
+    // call safe proxy to get nonce
+    bytes memory data = abi.encodeWithSignature("nonce()");
+    (bool success, bytes memory result) = safeProxyAddress.staticcall(data);
+    if (!success) {
+      revert("Failed to get nonce");
+    }
+    return abi.decode(result, (uint256));
+  }
+
+  function getThreshold() external view returns (uint256) {
+    // call safe proxy to get threshold
+    bytes memory data = abi.encodeWithSignature("threshold()");
+    (bool success, bytes memory result) = safeProxyAddress.staticcall(data);
+    if (!success) {
+      revert("Failed to get threshold");
+    }
+    return abi.decode(result, (uint256));
+  }
+  
+}
